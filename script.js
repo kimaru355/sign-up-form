1226ee12ad1e33db56eb776be9f1e8f4dc623f9d
kimaru355 <emmanuelkimaru355@gmail.com>
2023-11-07 00:06:39 +0300

    form validation with regexp

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 function validate() {
-  firstName = document.querySelector("#first_name").value;
-  lastName = document.querySelector("#last_name").value;
-  email = document.querySelector("#email").value;
-  password = document.querySelector("#password").value;
-  confirmPassword = document.querySelector("#confirm_password").value;
-  let emailFormat = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
-  if (email.match(emailFormat)) {
-    alert("All Details are good");
-  } else {
-    alert("Confirm email");
+  let firstName = document.querySelector("#first_name").value;
+  let lastName = document.querySelector("#last_name").value;
+  let email = document.querySelector("#email").value;
+  let phoneNumber = document.querySelector("#number").value;
+  let password = document.querySelector("#password").value;
+  let confirmPassword = document.querySelector("#confirm_password").value;
+  let error = document.querySelector("#error");
+  const emailFormat = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
+  const phoneFormat = /^[\+]?[0-9]+$/;
+  error.textContent = "";
+  if (firstName === "") {
+    error.textContent += "Enter First Name. ";
+    return;
+  }
+  if (!email.match(emailFormat)) {
+    error.textContent += "Wrong Email format. ";
+    return;
+  }
+  if (!phoneNumber.match(phoneFormat)) {
+    error.textContent += "Phone number not valid. ";
+    return;
+  }
+  if (password === "" && confirmPassword === "") {
+    error.textContent += "Enter password. ";
+    return;
+  }
+  if (password !== confirmPassword) {
+    error.textContent += "Passwords do not match. ";
+    return;
+  }
+  if (error.textContent === "") {
+    error.textContent += "All details are good. ";
+    return;
   }
 }
